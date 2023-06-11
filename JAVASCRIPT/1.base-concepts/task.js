57cde70f62f82a50d80bde4eabbb560cb3fcb44c
Diablolend <Diablolend@gmail.com>
2023-06-11 22:10:12 +0500

    #

--- JAVASCRIPT/1.base-concepts/task.js
+++ JAVASCRIPT/1.base-concepts/task.js
@@ -1,10 +1,25 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b**2-4*a*c;
+  if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d))/(2*a);
+    arr[1] = (-b - Math.sqrt(d))/(2*a);
+  } else if (d == 0) {
+    arr[0] = -b/(2*a);
+  }
   return arr;
 }
+solveEquation(1, 5, 4)
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount)) {
+    return false;
+  }
+  let P = percent/100/12;
+  let S = amount - contribution;
+  let amountPerMonth = S * (P + (P / (((1 + P)**countMonths) - 1)));
+  let totalAmount = amountPerMonth * countMonths;
+  return Number(totalAmount.toFixed(2));
 }
+calculateTotalMortgage(10, 0, 10000, 36)
