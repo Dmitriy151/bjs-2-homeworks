360344ce8ee8cf8bec3203ef1bab0f61ed77f019
Dmitriy151 <nova151kra@gmail.com>
2023-06-21 16:27:17 +0600

    21.06

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
+  let d=Math.pow(b, 2)-4*a*c;
+  //console.log('d='+d);
   let arr = [];
+  if (d>0) {
+    arr[0]=(-b + Math.sqrt(d) )/(2*a); 
+    arr[1]=(-b - Math.sqrt(d) )/(2*a);
+    console.log(arr);
+    return arr;
+  }else if (d===0) {
+    arr[0]=-b/(2*a);
+    console.log(arr);
+    return arr;
+  }else if(d<0){
+    arr[0]=undefined;
+    console.log(arr);
+    return arr;
+  };
   
-  return arr;
+
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+solveEquation();
+// let result;
+// result=arr;
+// console.log(result);
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {};
