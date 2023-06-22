d33021681961e930d421911496f6f5eeabb59a60
Dmitriy151 <nova151kra@gmail.com>
2023-06-22 17:13:55 +0600

    22.06.1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,20 +1,19 @@
 "use strict";
 function solveEquation(a, b, c) {
   let d=Math.pow(b, 2)-4*a*c;
-  //console.log('d='+d);
+  console.log('d='+d);
   let arr = [];
   if (d>0) {
     arr[0]=(-b + Math.sqrt(d) )/(2*a); 
     arr[1]=(-b - Math.sqrt(d) )/(2*a);
-    //console.log(arr);
+    console.log(arr);
     return arr;
   }else if (d===0) {
     arr[0]=-b/(2*a);
-    //console.log(arr);
+    console.log(arr);
     return arr;
   }else if(d<0){
-    //arr[0]=undefined;
-    //console.log(arr);
+    console.log(arr);
     return arr;
   };
   
@@ -22,8 +21,16 @@ function solveEquation(a, b, c) {
 }
 
 solveEquation();
-// let result;
-// result=arr;
-// console.log(result);
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {};
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let s=amount;
+  let p=percent/12*100;
+  let n=countMonths;
+  let payment = s * (p + (p / (((1 + p)**n) - 1)));
+  let sum= payment*n;
+  console.log (sum);
+  console.log (payment);
+  return sum;
+};
+
+calculateTotalMortgage(10, 0, 50000, 12);
