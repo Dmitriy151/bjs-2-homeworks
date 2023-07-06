bff0852f606ab669f50f834797e68e4b6f39534f
Dmitriy151 <nova151kra@gmail.com>
2023-07-06 16:37:41 +0600

    06.07

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,18 @@
 function getArrayParams(...arr) {
-
+  min =  Infinity;
+  max = -Infinity;
+  sum=0;
+  for (let index = 0; i < arr.length; index++) {
+    const element = arr[i];
+    sum+=arr[i];
+    if (arr[i]>max) {
+      //sum+=arr[i];
+      max=arr[i];
+    }if (arr[i]<min) {
+      min=arr[i];
+    }
+    
+  }
   return { min: min, max: max, avg: avg };
 }
 
