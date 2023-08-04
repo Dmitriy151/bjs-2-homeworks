390e07d64e312f5c186e2aa572cdc5ecb780031e
Dmitriy151 <nova151kra@gmail.com>
2023-08-04 16:21:46 +0600

    04.08

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
+//*(arr1.elem===arr2.elem, arr1.index===arr2.index,arr1===arr2)
 function compareArrays(arr1, arr2) {
-  
+  if (arr1===arr2) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
+console.log(compareArrays([1,2,3], [1,2,3]));
 function getUsersNamesInAgeRange(users, gender) {
   
 }
