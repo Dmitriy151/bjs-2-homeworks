25214ee3fc015b043137482e05e862de5d847c9b
Dmitriy151 <nova151kra@gmail.com>
2023-09-04 19:52:08 +0600

    04.09

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,53 +1,53 @@
 class AlarmClock {
-  constructor() {
-      this.alarmCollection = [];
-      this.intervalId = null;
-  }
+	constructor() {
+			this.alarmCollection = [];
+			this.intervalId = null;
+	}
 
-  addClock(time, callback) {
-      if(!time || !callback) {
-          throw new Error("Отсутствуют обязательные аргументы");
-      } else if(this.alarmCollection.some((alarm) => alarm.time === time)) {
-          console.warn("Уже присутствует звонок на это же время");
-      }
-      this.alarmCollection.push( {time, callback, canCall: true} );
-  }
+	addClock(time, callback) {
+			if(!time || !callback) {
+					throw new Error("Отсутствуют обязательные аргументы");
+			} else if(this.alarmCollection.some((alarm) => alarm.time === time)) {
+					console.warn("Уже присутствует звонок на это же время");
+			}
+			this.alarmCollection.push( {time, callback, canCall: true} );
+	}
 
-  removeClock(time) {
-      this.alarmCollection = this.alarmCollection.filter((alarm) => alarm.time !== time);
-  }
+	removeClock(time) {
+			this.alarmCollection = this.alarmCollection.filter((alarm) => alarm.time !== time);
+	}
 
-  getCurrentFormattedTime() {
-      return new Date().toLocaleTimeString([], 
-      {
-        hour: "2-digit",
-        minute: "2-digit",
-      });
-  }
+	getCurrentFormattedTime() {
+			return new Date().toLocaleTimeString([], 
+			{
+				hour: "2-digit",
+				minute: "2-digit",
+			});
+	}
 
-  start() {
-      if(this.intervalId) {
-          return;
-      }
-      this.intervalId = setInterval( () => this.alarmCollection.forEach((alarm) => {
-          if(alarm.time === this.getCurrentFormattedTime() && alarm.canCall) {
-              alarm.canCall = false;
-              alarm.callback();
-          }
-      }), 1000)
-  }
+	start() {
+			if(this.intervalId) {
+					return;
+			}
+			this.intervalId = setInterval( () => this.alarmCollection.forEach((alarm) => {
+					if(alarm.time === this.getCurrentFormattedTime() && alarm.canCall) {
+							alarm.canCall = false;
+							alarm.callback();
+					}
+			}), 1000)
+	}
 
-  stop() {
-      clearInterval(this.intervalId);
-      this.intervalId = null;
-  }
+	stop() {
+			clearInterval(this.intervalId);
+			this.intervalId = null;
+	}
 
-  resetAllCalls() {
-      this.alarmCollection.forEach((alarm) => (alarm.canCall = true))
-  }
+	resetAllCalls() {
+			this.alarmCollection.forEach((alarm) => (alarm.canCall = true))
+	}
 
-  clearAlarms() {
-      this.stop();
-      this.alarmCollection = [];
-  }
+	clearAlarms() {
+			this.stop();
+			this.alarmCollection = [];
+	}
 }
